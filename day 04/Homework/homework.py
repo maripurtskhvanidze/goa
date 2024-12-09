7d7773461a9ff2774e941b935278d8a2f36b0ef9
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-12-10 00:39:39 +0400

    commit

---
+++ day 04/Homework/homework.py
@@ -0,0 +1,48 @@
+print(5 + 3)
+print(8 + 2)
+print(9 + 7)
+
+print(8 - 4)
+print(7 - 5)
+print(3 - 3)
+
+print(8 * 7)
+print(5 * 3)
+print(1 * 6)
+
+print(9 / 3)
+print(6 / 2)
+print(32 / 2)
+
+print(8 % 3)
+print(99 % 4)
+print(63 % 5)
+
+print(9 ** 5)
+print(5 ** 2)
+print(7 ** 8)
+
+
+celsius=15
+fahrenheit=(15 * 9/5) + 32 
+
+
+age1=33
+age2=9
+age3=70
+age4=67
+age5=15
+
+#20 years later
+
+age1=53
+age2=29
+age3=90
+age4=87
+age5=35
+
+print(age1)
+print(age2)
+print(age3)
+print(age4)
+print(age5)
