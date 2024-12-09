9e46d15f06cfb040793f00e0216cfd90e56e37d1
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-12-10 00:40:25 +0400

    commit

---
+++ day 06/Classwork/classwork.py
@@ -0,0 +1,89 @@
+x=10
+
+x -= 3
+print(x)
+
+x -= 6
+print(x)
+
+x -=8
+print(x)
+
+x -=7
+print(x)
+
+x -= 3
+print(x)
+
+y=5
+
+y += 5
+print(y)
+
+y += 3
+print(y)
+
+y += 2
+print(y)
+
+y += 1
+print(y)
+
+y += 2
+print(y)
+
+
+a=7
+
+a /= 3
+print(a)
+
+a /= 5
+print(a)
+
+a /= 7
+print(a)
+
+a /= 2
+print(a)
+
+a /= 4
+print(a)
+
+
+b=9
+
+b *= 3
+print(b)
+
+b *= 7
+print(b)
+
+b *= 2
+print(b)
+
+b *= 8
+print(b)
+
+b *= 5
+print(b)
+
+
+print("Hello World")
+print("variable")
+print("Hello World")
+print("This is an indented line")
+print("undeclared_variable")
+print("class")
+print("Hello World")
+x = 10
+y = 5
+print(x + y)
+print("Missing indentation in function")
+a =7
+b =10
+print(a + b)
+print("Unclosed string")
+print("Escaped quote" )
+print("No colon in if statement")
+print("Empty statement after variable declaration")
