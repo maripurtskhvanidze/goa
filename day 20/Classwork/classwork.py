e1fc7f177626094d0a732ac896b4cf1930786232
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2024-12-17 01:29:53 +0400

    commit

---
+++ day 20/Classwork/classwork.py
@@ -0,0 +1,17 @@
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+num1=list1[0]
+num2=list1[1]
+num3=list1[2]
+num4=list1[3]
+num5=list1[4]
+num6=list1[5]
+num7=list1[6]
+num8=list1[7]
+num9=list1[8]
+num10=list1[9]
+
+num1=int(input("enter number: "))
+num2=int(input("enter number: "))
+num3=int(input("enter number: "))
+list1=[num1, num2, num3]
+print(list1)
---
+++ day 20/Homework/homework.py
@@ -0,0 +1,40 @@
+num1=int(input("enter first number: "))
+num2=int(input("enter second number; "))
+num3=int(input("enter third number "))
+
+largest=(num1, num2, num3)
+print("the largest number", largest)
+
+
+score=int(input("enter the grade: "))
+
+if 90 <= score <= 100:
+    print("A")
+elif 80 <= score <= 89:
+    print("B")
+elif 70 <= score <= 79:
+    print("C")
+elif 60 <= score <= 69:
+    print("D")
+else:
+    print("F")
+
+
+number=int(input("enter number: "))
+
+if number >= 0:
+    print("number is positive")
+elif number <= 0:
+    print("number is negative")
+else:
+    print("number is zero")
+
+
+numbers=[1, 2, 3, 4, 5]
+print("first number" [0])
+print("third number" [2])
+print("fifth number" [4])
+
+
+
+
