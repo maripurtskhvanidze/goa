df0b6fbf890c00064efce3b80cacf0a192d0a0c0
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2025-01-10 19:49:55 +0400

    commit

---
+++ day 24/Homework/homework.py
@@ -0,0 +1,14 @@
+list1=[1, 2, 3, 4, 5]
+first_element=list1[0]
+print(first_element)
+
+
+list2=[1, 2, 3, 4, 5]
+last_element=list2[-1]
+print(last_element)
+
+
+first="hello world"
+last=first[-5:]
+print(last)
+
