e7738a2d09d3192ea24ee5ce8060f29b2fecca41
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2025-01-22 20:50:48 +0400

    commit

---
+++ day 30/Classwork/classwork.py
@@ -0,0 +1,7 @@
+main_str=input("enter main string: ")
+str_to_count=input("enter count string: ")
+count=main_str.count(str_to_count)
+print(count)
+
+str1 = "LORem IPSum"
+print(str1.swapcase())
---
+++ day 30/Homework/homework.py
@@ -0,0 +1,12 @@
+sentence=input("enter sentence: ")
+print(sentence.upper())
+
+name=input("enter your name: ")
+print(name.upper())
+
+
+sentence="HALA MADRID"
+print(sentence.lower())
+
+email=input("enter your email: ")
+print(email.lower())
