14bc18debce687f64864791162069288c4a8b07f
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2025-02-13 15:06:16 +0400

    commit

---
+++ day 38/Classwork/classwork.py
@@ -0,0 +1,4 @@
+tuple= ("apple", "banana", "orange", "peach", "mango", "kiwi", "pear," "watermelon", "melon", "berry")
+for i in range(len(tuple)):
+    print(f"{i} {tuple}")
+
---
+++ day 38/Homework/homework.py
@@ -0,0 +1,5 @@
+set ={1, 2, 3, 4, 5}
+set.add(6)
+set.remove(3)
+print(4 in set)
+print(set)
