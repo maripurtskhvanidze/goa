549fd605c3ce748f429ecfde090ea4085c8ceae4
maripurtskhvanidze <maripurtskhvanidze49@gmail.com>
2025-03-03 23:53:34 +0400

    commit

---
+++ day 42/Classwork/classwork.py
@@ -0,0 +1,13 @@
+information = {
+    "name" = "Mari",
+    "surname" = "Purtskhvanidze",
+    "academy" = "GOA",
+    "fav-color" = "black",
+    "city" = "Tbilisi",
+    "GOA-student" = "true",
+    "age" = 15,
+    "fav-programming-lang" = "Python"
+}
+
+for key, value in information.items():
+    print(f"{key}: {value}")
---
+++ day 42/Homework/homework.py
@@ -0,0 +1,13 @@
+dictionary = {
+    "brend" ="BMW"
+    "model" ="e36"
+    "year" = 1995
+    "color" = "black"
+}
+print(dictionary)
+
+dict= {"apple", "pear", "peach", "grape"}
+
+for key, value in dict.items():
+    print(f{key}: {value})
+
